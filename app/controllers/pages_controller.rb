d438bbdf0105239a18a72d56e1fa66b2769b9d61
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-06-13 16:03:39 -0400

    Define @header for each instance method of Pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
   def welcome
+    @header = 'This is the welcome page'
     render :welcome
   end
 
   def about
+    @header = 'This is the about page'
     render :about
   end
 
   def contest
+    @header = 'This is the contest page'
   end
 end
